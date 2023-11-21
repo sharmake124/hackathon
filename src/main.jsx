61cbed790f8949c9ff31f8652abd8d02c3229a9c
Christopher Arthaud <christopher.arthaud@gmail.com>
2023-11-21 23:53:40 +0100

    route created

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,22 @@
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
+import Game from "./pages/game/Game.jsx";
+import React from "react";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/game",
+    element: <Game />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
