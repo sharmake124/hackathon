6f5af8ce64a7acc32bf5d1effecb3e2f95c48d1c
Alexis_Lienard <lienard.alexis@gmail.com>
2023-11-23 17:50:36 +0100

    first commit

---
+++ src/components/Input.jsx
@@ -0,0 +1,16 @@
+import "./styles/Input.css"
+
+export default function Input() {
+  return (
+    <div className="input">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum quia
+        voluptatibus fugit, doloribus enim cum ipsam unde fugiat error
+        exercitationem sed accusantium at qui porro velit modi magni dolorem!
+        Perspiciatis quaerat et modi. Adipisci fuga eius perspiciatis minima
+        suscipit corrupti totam non dolorum voluptate architecto! Iure itaque
+        officiis fugit deserunt?
+      </p>
+    </div>
+  );
+}
---
+++ src/components/Prompt.jsx
@@ -0,0 +1,16 @@
+import "./styles/Prompt.css"
+
+export default function Prompt() {
+  return (
+    <div className="prompt">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum quia
+        voluptatibus fugit, doloribus enim cum ipsam unde fugiat error
+        exercitationem sed accusantium at qui porro velit modi magni dolorem!
+        Perspiciatis quaerat et modi. Adipisci fuga eius perspiciatis minima
+        suscipit corrupti totam non dolorum voluptate architecto! Iure itaque
+        officiis fugit deserunt?
+      </p>
+    </div>
+  );
+}
