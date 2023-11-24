6d1403ff75d278dcf7dc1a081d98a68e6f3a8b4c
Messaoud ZOUGGAB <messaoud.zouggab@gmail.com>
2023-11-24 09:34:56 +0100

    ajout du dé

---
+++ src/components/Dice.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import "./Dice.css";
+
+function Dice() {
+  const [diceOne, setDiceOne] = useState();
+
+  function rollDice() {
+    const newDiceValue = Math.floor(Math.random() * 6) + 1;
+    setDiceOne(newDiceValue);
+  }
+
+  return (
+    <div>
+      <div id="dice1" className={`dice show-${diceOne}`} onClick={rollDice}>
+        <div id="dice1" className="dice dice-one">
+          <div id="dice-one-side-one" className="side one">
+            <div className="dot one-1"></div>
+          </div>
+          <div id="dice-one-side-two" className="side two">
+            <div className="dot two-1"></div>
+            <div className="dot two-2"></div>
+          </div>
+          <div id="dice-one-side-three" className="side three">
+            <div className="dot three-1"></div>
+            <div className="dot three-2"></div>
+            <div className="dot three-3"></div>
+          </div>
+          <div id="dice-one-side-four" className="side four">
+            <div className="dot four-1"></div>
+            <div className="dot four-2"></div>
+            <div className="dot four-3"></div>
+            <div className="dot four-4"></div>
+          </div>
+          <div id="dice-one-side-five" className="side five">
+            <div className="dot five-1"></div>
+            <div className="dot five-2"></div>
+            <div className="dot five-3"></div>
+            <div className="dot five-4"></div>
+            <div className="dot five-5"></div>
+          </div>
+          <div id="dice-one-side-six" className="side six">
+            <div className="dot six-1"></div>
+            <div className="dot six-2"></div>
+            <div className="dot six-3"></div>
+            <div className="dot six-4"></div>
+            <div className="dot six-5"></div>
+            <div className="dot six-6"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Dice;
